19eb9df64b81386d0e56374bdacb2ce2d9c2e783
Crocodile <wwwblogger1@gmail.com>
2023-02-09 21:25:47 +0200

    add 2 files to Generics_4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace Lesson
+{
+    // обобщённый List
+    // подключать using System.Collections.Generic
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string>list = new List<string>(); // так- же в List можно хранить class and struct
+            List<int> list = new List<int>(); // List - как готовый массив в котором хранится тип данных int
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list[0]);
+            list[0] = 2;
+        }
+    }
+}
